f6d18bfa8e25c196691523d8d2f1df843091d630
Aayush Razdan <razdanaayush19@gmail.com>
2020-10-25 04:08:45 +0530

    added photo for motivation

--- restaurant-review-react/src/App.js
+++ restaurant-review-react/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import Navbar from "./components/navbar.component";
 
@@ -8,6 +10,20 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <header className="jumbotron">
+        <div className="container">
+          <h1>
+            <span className="glyphicon glyphicon-tent"></span> Welcome To
+            Restaurant Review
+          </h1>
+          <p>By Quarantine Lovers</p>
+          <p>
+            <Link to="" className="btn btn-primary btn-md">
+              Add New Restaurant
+            </Link>
+          </p>
+        </div>
+      </header>
     </Router>
   );
 }
