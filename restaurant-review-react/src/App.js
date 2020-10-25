1a815113d7a54ac91105cc38ce74491fad66c63b
Saksham Tikoo <tikoosaksham@gmail.com>
2020-10-25 14:01:39 +0530

    Basic form added

--- restaurant-review-react/src/App.js
+++ restaurant-review-react/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 import Navbar from "./components/navbar.component";
+import RestaurantList from "./components/restaurantList.component";
 
 function App() {
   return (
@@ -13,14 +14,14 @@ function App() {
       <header className="jumbotron">
         <div className="container">
           <h1>
-            <span className="glyphicon glyphicon-tent"></span> Welcome To
-            Restaurant Review
+            <span className="glyphicon glyphicon-tent"></span> Welcome To Review
           </h1>
           <p>By Quarantine Lovers</p>
           <p>
-            <Link to="" className="btn btn-primary btn-md">
+            <Link to="/RestaurantList" className="btn btn-primary btn-md">
               Add New Restaurant
             </Link>
+            <Route path="/RestaurantList" component={RestaurantList} exact />
           </p>
         </div>
       </header>
