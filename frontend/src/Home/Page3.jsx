e3f9242acd81a49d1405f4c42264e9304f1395ae
ayushpattnaik <pattnaikayush21@gmail.com>
2021-01-15 23:31:23 +0530

    frontend website

---
+++ frontend/src/Home/Page3.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TweenOne from 'rc-tween-one';
+import ScrollOverPack from 'rc-scroll-anim/lib/ScrollOverPack';
+import { Icon, Button } from 'antd';
+import QueueAnim from 'rc-queue-anim';
+
+export default function Page3({ isMobile }) {
+  return (
+    <ScrollOverPack id="page3" className="content-wrapper page">
+      <TweenOne
+        key="image"
+        className="image3 image-wrapper"
+        animation={{ x: 0, opacity: 1, ease: 'easeOutQuad' }}
+        style={{ transform: 'translateX(-100px)', opacity: 0 }}
+      />
+      <QueueAnim
+        className="text-wrapper"
+        key="text"
+        type={isMobile ? 'bottom' : 'right'}
+        leaveReverse
+        style={{ top: '40%' }}
+      >
+        <h2 key="h2">Restaurants</h2>
+        <p key="p" style={{ maxWidth: 280 }}>
+                      Feelin' bored at home? Let's explore some places for you to chill and eat!         </p>
+        <div key="button">
+          <a>
+            <Button type="primary" size="large">
+              Check out Explore section
+              <Icon type="right" />
+            </Button>
+          </a>
+        </div>
+      </QueueAnim>
+    </ScrollOverPack>
+  );
+}
+Page3.propTypes = {
+  isMobile: PropTypes.bool,
+};
