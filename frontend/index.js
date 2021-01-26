6991926af3a505dac5f4e10d7fb4a2a1ad45f8a7
Aayush Razdan <razdanaayush19@gmail.com>
2021-01-26 13:29:12 +0530

    .

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './src/Home';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./src/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CreateRestaurant from "./src/Home/createRestaurant";
+import RestaurantList from "./src/Home/RestaurantList";
 
 function App() {
   return (
-    <Home />
+    <Router>
+      {/* <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div> */}
+      <Route path="/" exact component={Home} />
+      <Route path="/CreateRestaurant" component={CreateRestaurant} />
+      <Route path="/RestaurantList" component={RestaurantList} />
+    </Router>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
--- frontend/src/Home/Header.jsx
+++ frontend/src/Home/Header.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import orderFood  from "./orderFood";
-import { Menu, Row, Col, Icon, Button, Popover, Badge } from 'antd';
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import orderFood from "./orderFood";
+import { Menu, Row, Col, Icon, Button, Popover, Badge } from "antd";
+import { Link } from "react-router-dom";
 
-
-const searchEngine = 'Google';
+const searchEngine = "Google";
 
 export default class Header extends React.Component {
   static propTypes = {
     isFirstScreen: PropTypes.bool,
     isMoblie: PropTypes.bool,
-  }
+  };
   state = {
     menuVisible: false,
   };
@@ -19,71 +19,73 @@ export default class Header extends React.Component {
     this.setState({
       menuVisible: visible,
     });
-  }
+  };
   handleShowMenu = () => {
     this.setState({
       menuVisible: true,
     });
-  }
+  };
 
   handleHideMenu = () => {
     this.setState({
       menuVisible: false,
     });
-  }
+  };
 
   handleSelectFilter = (value, option) => {
-    const optionValue = option.props['data-label'];
-    return optionValue === searchEngine ||
-      optionValue.indexOf(value.toLowerCase()) > -1;
-  }
+    const optionValue = option.props["data-label"];
+    return (
+      optionValue === searchEngine ||
+      optionValue.indexOf(value.toLowerCase()) > -1
+    );
+  };
 
   render() {
     const { isFirstScreen, isMoblie } = this.props;
     const { menuVisible } = this.state;
-    const menuMode = isMoblie ? 'inline' : 'horizontal';
+    const menuMode = isMoblie ? "inline" : "horizontal";
     const headerClassName = classNames({
       clearfix: true,
-      'home-nav-white': !isFirstScreen,
+      "home-nav-white": !isFirstScreen,
     });
 
     const menu = [
       <Button className="header-lang-button" ghost size="small" key="lang">
         Sign Up
       </Button>,
-      <Menu mode={menuMode} defaultSelectedKeys={['home']} id="nav" key="nav">
+      <Menu mode={menuMode} defaultSelectedKeys={["home"]} id="nav" key="nav">
         <Menu.Item key="home">
-        <a
+          <a
             href="/home"
             className="header-link"
             // target="_blank"
             rel="noopener noreferrer"
             component={orderFood}
           >
-          HOME
+            HOME
           </a>
         </Menu.Item>
         <Menu.Item key="docs/spec">
-        <a
-            href="/restaurants"
+          <Link
+            to="/RestaurantList"
             className="header-link"
             // target="_blank"
-            rel="noopener noreferrer"
-            component={orderFood}
+            //rel="noopener noreferrer"
+            // component={orderFood}
           >
-          RESTAURANTS
-          </a>
+            SEARCH
+          </Link>
         </Menu.Item>
         <Menu.Item key="docs/react">
-        <a
-            href="/blog"
+          <Link
+            to="/CreateRestaurant"
             className="header-link"
             // target="_blank"
-            rel="noopener noreferrer"
-            component={orderFood}
+            //rel="noopener noreferrer"
+            // component={orderFood}
           >
-          BLOG
-          </a>
+            ADD
+          </Link>
         </Menu.Item>
         <Menu.Item key="docs/pattern">
           <a
@@ -93,7 +95,7 @@ export default class Header extends React.Component {
             rel="noopener noreferrer"
             component={orderFood}
           >
-          ORDER FOOD
+            ORDER FOOD
           </a>
         </Menu.Item>
         {}
@@ -118,7 +120,7 @@ export default class Header extends React.Component {
 
     return (
       <header id="header" className={headerClassName}>
-        {menuMode === 'inline' ? (
+        {menuMode === "inline" ? (
           <Popover
             overlayClassName="popover-menu"
             placement="bottomRight"
@@ -138,12 +140,15 @@ export default class Header extends React.Component {
         <Row>
           <Col lg={4} md={4} sm={24} xs={24}>
             <a id="logo">
-              <img alt="logo" src="https://cdn.dribbble.com/users/3306608/screenshots/9042257/media/1b9a44ea9f560143c0f665b39508d12d.jpg" />
+              <img
+                alt="logo"
+                src="https://cdn.dribbble.com/users/3306608/screenshots/9042257/media/1b9a44ea9f560143c0f665b39508d12d.jpg"
+              />
               <span>Noobs</span>
             </a>
           </Col>
           <Col lg={20} md={19} sm={0} xs={0}>
-            {menuMode === 'horizontal' ? menu : null}
+            {menuMode === "horizontal" ? menu : null}
           </Col>
         </Row>
       </header>
