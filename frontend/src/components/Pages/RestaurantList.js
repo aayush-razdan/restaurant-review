d8d54e93801f7966201d16fb1244889881362fa5
Saksham Tikoo <tikoosaksham@gmail.com>
2021-01-27 12:09:06 +0530

    introduced cards for restaurant list

--- frontend/src/components/Pages/RestaurantList.js
+++ frontend/src/components/Pages/RestaurantList.js
@@ -3,8 +3,57 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "../stylesheets/RestaurantList.css";
+// import { Card } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // maxWidth: 345,
+    width: 200
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
 
 const Restaurant = (props) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
     <div class="col-md-3 col-sm-6">
       <div className="thumbnail">
@@ -12,14 +61,49 @@ const Restaurant = (props) => {
         <div className="caption">
           <h4>{props.restaurant.resname}</h4>
         </div>
-        <p>
-          <Link
+        <Card className={classes.root}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="recipe" className={classes.avatar}>
+                R
+          </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+          />
+          <CardMedia
+            className={classes.media}
+            image=""
+            title="Restaurant"
+          />
+          {/* <Link
             to={"/view/" + props.restaurant._id}
             className="btn btn-primary"
-          >
-            More
-          </Link>
-        </p>
+          > */}
+          {props.restaurant.description}
+          {/* </Link> */}
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
       </div>
     </div>
   );
