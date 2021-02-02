3389bfbe01a30dcaa9ef2a23676334e7007b33c9
Aayush Razdan <razdanaayush19@gmail.com>
2021-02-02 17:55:42 +0530

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/nav";
 import Home from "./components/Pages/HomeComponent";
 import CreateRestaurant from "./components/Pages/createRestaurant";
 import RestaurantList from "./components/Pages/RestaurantList";
+import CreateUser from "./components/Pages/CreateUser";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path="/" exact component={Home} />
       <Route path="/CreateRestaurant" component={CreateRestaurant} />
       <Route path="/RestaurantList" component={RestaurantList} />
+      <Route path="/CreateUser" component={CreateUser} />
     </Router>
   );
 }
--- frontend/src/components/Pages/CreateUser.js
+++ frontend/src/components/Pages/CreateUser.js
@@ -1,43 +1,51 @@
 import React, { ReactDOM, Component } from "react";
 //import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from "../App.js"
-import { Card, Icon, Image } from 'semantic-ui-react'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Card, Icon, Image } from "semantic-ui-react";
 
 export default class CreateUser extends Component {
-    render() {
-        return (
-            //<Router>
-            // <nav className="navbar navbar bg-dark navbar-expand-lg">
-            <Card>
-                <form>
-                    <p>
-                        <b>Your name: </b>
-                        <input type="text"></input>
-                    </p>
-                    <p>{"\n"}</p>
-                    <p>
-                        <b>Username: </b>
-                        <input type="text"></input>
-                    </p>
-                    <p>{"\n"}</p>
-                    <p>
-                        <b>Email id: </b>
-                        <input type="text"></input>
-                    </p>
-                </form>
-                {/* <Card> */}
-                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-                <Card.Content>
-                    {/* <Card.Header>Matthew</Card.Header> */}
-                    <Card.Meta>
-                        <span className='date'>Joined in 2015</span>
-                    </Card.Meta>
-                </Card.Content>
-            </Card>
-        );
-        {/* </nav> */ }
-        {/* </Router> */ }
+  render() {
+    return (
+      //<Router>
+      // <nav className="navbar navbar bg-dark navbar-expand-lg">
+      <Card>
+        <form>
+          <p>
+            <b>Your name: </b>
+            <input type="text"></input>
+          </p>
+          <p>{"\n"}</p>
+          <p>
+            <b>Username: </b>
+            <input type="text"></input>
+          </p>
+          <p>{"\n"}</p>
+          <p>
+            <b>Email id: </b>
+            <input type="text"></input>
+          </p>
+        </form>
+        {/* <Card> */}
+        <Image
+          src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          {/* <Card.Header>Matthew</Card.Header> */}
+          <Card.Meta>
+            <span className="date">Joined in 2015</span>
+          </Card.Meta>
+        </Card.Content>
+      </Card>
+    );
+    {
+      /* </nav> */
     }
+    {
+      /* </Router> */
+    }
+  }
 }
