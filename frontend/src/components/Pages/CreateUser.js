e5f40452f8f776b9983a69faa0d8c6c7b3f1cbcf
Aayush Razdan <razdanaayush19@gmail.com>
2021-02-02 18:02:27 +0530

    .

--- frontend/src/components/Pages/CreateUser.js
+++ frontend/src/components/Pages/CreateUser.js
@@ -1,22 +1,13 @@
 import React, { ReactDOM, Component, Text } from "react";
 //import { BrowserRouter as Router, Route } from "react-router-dom";
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import { Card, Icon, Image } from "semantic-ui-react";
-=======
 import { Link, Router } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Icon, Image } from 'semantic-ui-react'
->>>>>>> b2ba9d008cfb70189e61c475e61b1b390708e921
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Icon, Image } from "semantic-ui-react";
 
 export default class CreateUser extends Component {
-<<<<<<< HEAD
   render() {
     return (
-      //<Router>
+      // <Router>
       // <nav className="navbar navbar bg-dark navbar-expand-lg">
       <Card>
         <form>
@@ -50,45 +41,10 @@ export default class CreateUser extends Component {
       </Card>
     );
     {
-      /* </nav> */
+      /* <Text>Pattnaik bsdka</Text> */
     }
     {
       /* </Router> */
-=======
-    render() {
-        return (
-            // <Router>
-            // <nav className="navbar navbar bg-dark navbar-expand-lg">
-            <Card>
-                <form>
-                    <p>
-                        <b>Your name: </b>
-                        <input type="text"></input>
-                    </p>
-                    <p>{"\n"}</p>
-                    <p>
-                        <b>Username: </b>
-                        <input type="text"></input>
-                    </p>
-                    <p>{"\n"}</p>
-                    <p>
-                        <b>Email id: </b>
-                        <input type="text"></input>
-                    </p>
-                </form>
-                {/* <Card> */}
-                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-                <Card.Content>
-                    {/* <Card.Header>Matthew</Card.Header> */}
-                    <Card.Meta>
-                        <span className='date'>Joined in 2015</span>
-                    </Card.Meta>
-                </Card.Content>
-            </Card>
-        );
-            {/* <Text>Pattnaik bsdka</Text> */}
-            {/* </Router> */}
->>>>>>> b2ba9d008cfb70189e61c475e61b1b390708e921
     }
   }
 }
